3a39ca75ae3e8a84b77135bc416de16e904fe37f
Chatakondasaimadhav <147918415+Chatakondasaimadhav@users.noreply.github.com>
2025-09-21 01:18:19 +0530

    Create constants.js

---
+++ project-root/frontend/src/utils/constants.js
@@ -0,0 +1,3 @@
+export const REGIONS = ['North', 'South', 'East', 'West'];
+export const CATEGORIES = ['Electronics', 'Clothing', 'Home', 'Books'];
+export const CUSTOMER_TYPES = ['Retail', 'Wholesale'];
